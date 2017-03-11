ae2b99abcab15b4f6aeaf328e5dbf68e262fec21
weduoo <619069915@qq.com>
2017-03-11 11:41:08 +0800

    完善代码

--- hadoop-mymr/src/main/java/com/weduoo/test_mapreduce/wordcount/WordCountDriver.java
+++ hadoop-mymr/src/main/java/com/weduoo/test_mapreduce/wordcount/WordCountDriver.java
@@ -14,36 +14,40 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class WordCountDriver {
 
+	public static String LOCAL = "/Users/weduoo/test/mapreduce";
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		
 		Configuration conf = new Configuration();
-		System.setProperty("HADOOP_USER_NAME", "root");
-		conf.set("fs.defaultFS", "hdfs://192.168.11.101:9000");
-		conf.set("mapreduce.framework.name", "yarn");
-		conf.set("yarn.resourcemanager.hostname", "master");
-		
+//		System.setProperty("HADOOP_USER_NAME", "root");
+//		conf.set("fs.defaultFS", "hdfs://192.168.11.101:9000");
+//		conf.set("mapreduce.framework.name", "yarn");
+//		conf.set("yarn.resourcemanager.hostname", "master");
 		
 		Job job = Job.getInstance(conf);
 		
-		//job.setJar("/Users/weduoo/test/hadoop/wordcount.jar");
-		job.setJarByClass(WordCountDriver.class);
-		
-		job.setMapperClass(WordCountMapper.class);
-		job.setReducerClass(WordCountReducer.class);
-		
-		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(IntWritable.class);
-		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		FileInputFormat.setInputPaths(job, new Path("/wordcount/input/"));
-		FileOutputFormat.setOutputPath(job, new Path("/wordcount/output/"));
-		
-		boolean res = job.waitForCompletion(true);
-		System.exit(res?0:1);
+        //设置运行的主类
+        job.setJarByClass(WordCountDriver.class);
+        //设置Mapper和Reducer类
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        //设置输入输出
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //设置业务逻辑Reducer类的输出key和value的数据类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //设置输入输出处理方式
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        //设置输入输出路径，setInputPaths方法可以设置多个输入路径
+        FileInputFormat.setInputPaths(job, new Path(LOCAL+"/wordcount/input/"));
+        FileOutputFormat.setOutputPath(job, new Path(LOCAL+"/wordcount/output/"));
+
+        //向yarn集群提价job
+        //等待运行完成之后再退出，waitForCompletion的参数表示是否打印处理过程
+        //job运行成功返回true
+        System.exit(job.waitForCompletion(true)?0:1);
+
 	}
 }
